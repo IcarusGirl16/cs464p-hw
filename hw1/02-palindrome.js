bc8ab349c6457dca4cf90d009029c42c8c2d1658
IcarusGirl16 <135085812+IcarusGirl16@users.noreply.github.com>
2024-04-15 19:47:54 -0700

    completed exercise 2

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,25 @@
-const elem = document.querySelector('input');
+// Sources: https://stackoverflow.com/questions/38053729/javascript-how-to-reverse-a-number
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+let item = document.querySelector("#output");
+let out = document.createElement("div");
+//out.setAttribute("class", "message");
+item.append(out);
+
+function handleInput(event) {
+  if (elem.value <= 0) {
+    out.setAttribute("class", "text-danger");
+    out.textContent = "Please enter a positive number.";
+  } else {
+    let rev = elem.value.toString().split("").reverse().join("");
+    if (rev == elem.value) {
+      out.setAttribute("class", "text-success");
+      out.textContent = "Yes. This is a palindrome!";
+    } else {
+      out.setAttribute("class", "text-danger");
+      out.textContent = "No. Try again.";
+    }
+  }
+}
