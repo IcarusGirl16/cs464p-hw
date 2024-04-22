065edf239fee91a6d79a888b6daf9008056a05d4
IcarusGirl16 <135085812+IcarusGirl16@users.noreply.github.com>
2024-04-22 13:36:01 -0700

    added function to highlight text

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,18 @@
 // Add your code here
 
-input.addEventListener('keydown', handleKeyDown);
+userInput.addEventListener("keydown", handleKeyDown);
+const elem = document.querySelector("input");
+
+function handleKeyDown(event) {
+  let paragraph = document.getElementById("text");
+  let textToSearch = elem.value;
+  console.log(textToSearch);
+  textToSearch = textToSearch.replace(/[.*+?^${}()|[|]\\]/g, "\\$&");
+  let pattern = new RegExp(`${textToSearch}`, "gi");
+  if (textToSearch !== "" || textToSearch !== " ") {
+    paragraph.innerHTML = paragraph.textContent.replace(
+      pattern,
+      (match) => `<mark style="background-color: yellow">${match}</mark>`
+    );
+  }
+}
