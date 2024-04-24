82c0182b01b96b8a6f09c498288ee4a2d007c51c
IcarusGirl16 <135085812+IcarusGirl16@users.noreply.github.com>
2024-04-23 21:35:46 -0700

    started data collection

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,15 @@
 // Add your code here
+let fullname = document.querySelector("fullname");
+let email = document.querySelector("email");
+let regstatus = document.querySelector("regstat");
+let classlist = document.querySelectorAll('input[type="checkbox"]:checked');
+let thoughts = document.querySelector("other");
+
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  addToModal();
+});
+
+function addToModal() {
+  let popup = document.getElementsByClassName("modal-body");
+}
