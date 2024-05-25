e33611b78cc7d86d77c4d9fb178afa13b6810ef5
IcarusGirl16 <135085812+IcarusGirl16@users.noreply.github.com>
2024-05-24 19:03:55 -0700

    fixed contrast error

--- HW3_new/src/routes/root.tsx
+++ HW3_new/src/routes/root.tsx
@@ -1,10 +1,10 @@
 export default function Root() {
   return (
               <nav className='fixed top-0 w-full bg-gray-800 p-4 flex justify-right'>
-                <a href={`/main`} className="px-3">Home</a>
-                <a href={`/list`} className="px-3">List</a>
-                <a href={`/population`} className="px-3">Population</a>
-                <a href={`/custom`} className="px-3" >Languages</a>
+                <a href={`/main`} className="px-3 text-white">Home</a>
+                <a href={`/list`} className="px-3 text-white">List</a>
+                <a href={`/population`} className="px-3 text-white">Population</a>
+                <a href={`/custom`} className="px-3 text-white" >Languages</a>
               </nav>
   );
 }
